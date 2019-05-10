746424cd17f4dd17a0d359194e4876234ee3e651
Yulibar Husni <hyulibar@gmail.com>
2019-05-10 09:11:14 +0700

    Initial Commit

---
+++ Ice Cream Time/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ice Cream Time
+//
+//  Created by Yulibar Husni on 10/05/19.
+//  Copyright © 2019 Yulibar Husni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
